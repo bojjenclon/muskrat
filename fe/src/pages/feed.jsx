a985303dceafcec6c78ce84870f3d2e983d7aa70
Cornell Daly <bojjenclon@gmail.com>
2024-09-30 14:09:49 +0000

    Feed improvements

--- fe/src/pages/feed.jsx
+++ fe/src/pages/feed.jsx
@@ -28,6 +28,7 @@ const FeedPage = () => {
   
   const currentUser = baseStore.useStore((state) => state.user)
   const [userId, setUserId] = useState((typeof feedId === 'string') ? parseInt(feedId, 10) : currentUser.id)
+  const [viewedUser, setViewedUser] = useState(null)
   const isCurrentUser = userId === currentUser.id
 
   useEffect(() => {
@@ -53,6 +54,27 @@ const FeedPage = () => {
     
     firstLoad.current = false
   }
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const res = await fetchAuth(`${location.origin}/api/users/${userId}`, {
+        method: 'GET'
+      })
+
+      if (res.ok) {
+        const data = await res.json()
+        setViewedUser(data)
+      } else {
+        setViewedUser(null)
+      }
+    }
+    
+    fetchUser()
+      .catch((err) => {
+        console.error(err)
+        setViewedUser(null)
+      })
+  }, [isCurrentUser])
   
   useEffect(() => {
     const interval = setInterval(() => {
@@ -102,26 +124,36 @@ const FeedPage = () => {
   
   return (
     <Layout>
-      <div className='w-full flex border border-zinc-700 border-t-0 border-b-0'>
-        <button className='w-1/2 h-[48px] hover:bg-zinc-900 hover:cursor-pointer'>
-          <span className='text-slate-500'>For you</span>
-        </button>
-        <button className='w-1/2 h-[48px] hover:bg-zinc-900 hover:cursor-pointer'>
-          <span className='h-full inline-flex flex-col flex-between'>
-            <div className='flex-1 flex items-center'>
-              <span className='font-bold'>Following</span>
+      {isCurrentUser && (
+        <>
+          <div className='w-full flex border border-zinc-700 border-t-0 border-b-0'>
+            <button className='w-1/2 h-[48px] hover:bg-zinc-900 hover:cursor-pointer'>
+              <span className='text-slate-500'>For you</span>
+            </button>
+            <button className='w-1/2 h-[48px] hover:bg-zinc-900 hover:cursor-pointer'>
+              <span className='h-full inline-flex flex-col flex-between'>
+                <div className='flex-1 flex items-center'>
+                  <span className='font-bold'>Following</span>
+                </div>
+                <div className='w-auto h-[4px] bg-cyan-600 rounded-full'></div>
+              </span>
+            </button>
+          </div>
+        
+          <div className='flex flex-col gap-2 border border-zinc-700 border-b-0 p-[8px]'>
+            <Textarea className='bg-transparent resize-none !border-0 !border-transparent' id='post-content' placeholder={`What's on your mind?`} minLength={1} maxLength={255} rows={6} ref={postContentRef} />
+            <div className='w-full flex justify-end'>
+              <button className='px-[20px] py-[6px] bg-cyan-600 rounded-full font-bold' onClick={onSubmit}>Post</button>
             </div>
-            <div className='w-auto h-[4px] bg-cyan-600 rounded-full'></div>
-          </span>
-        </button>
-      </div>
-    
-      <div className='flex flex-col gap-2 border border-zinc-700 border-b-0 p-[8px]'>
-        <Textarea className='bg-transparent resize-none !border-0 !border-transparent' id='post-content' placeholder={`What's on your mind?`} minLength={1} maxLength={255} rows={6} ref={postContentRef} />
-        <div className='w-full flex justify-end'>
-          <button className='px-[20px] py-[6px] bg-cyan-600 rounded-full font-bold' onClick={onSubmit}>Post</button>
+          </div>
+        </>
+      )}
+
+      {(!isCurrentUser && viewedUser) && (
+        <div className='p-4 border border-zinc-700 border-t-0 border-b-0'>
+          <h1 className='text-xl'>Viewing <strong>{viewedUser.username}</strong>&apos;s feed</h1>
         </div>
-      </div>
+      )}
 
       <div className='flex flex-col'>
         {posts.map((post, idx) => (
